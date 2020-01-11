7d09c8dacfc9dca0464e369d6d5f9f5964059863
Hector Salazar <hecsalazarf@outlook.com>
2020-01-11 12:53:24 -0600

    feat(app): Add vuetify framework

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,21 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+const merge = require('webpack-merge')
+
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
+  plugins: [],
+  configureWebpack (config) {
+    return merge({
+      plugins: [
+        new VuetifyLoaderPlugin()
+      ]
+    }, config)
+  },
+  port: 8082
 }
--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import Vuetify from 'vuetify/lib'
+import '~/scss/main.scss'
+// import Vuetify from 'vuetify'
 
-export default function (Vue, { router, head, isClient }) {
+export default function (Vue, { router, appOptions, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  Vue.use(Vuetify)
+  appOptions.vuetify = new Vuetify({
+    icons: {
+      iconfont: 'fa'
+    }
+  })
 }
