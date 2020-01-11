35e957c3f043de9717d0029f525521bc3c37366f
Hector Salazar <hecsalazarf@outlook.com>
2020-01-11 12:54:00 -0600

    chore(style): Add eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+  }
+};
