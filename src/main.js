d7bc30681b59ab3895b627622b4986fb2bef69f9
Hector Salazar <hecsalazarf@outlook.com>
2020-01-12 13:20:48 -0600

    refactor(app): Add vuetify theme colors

--- src/main.js
+++ src/main.js
@@ -14,6 +14,28 @@ export default function (Vue, { router, appOptions, head, isClient }) {
     },
     lang: {
       current: 'es'
+    },
+    theme: {
+      dark: false,
+      default: 'light',
+      options: {
+        cspNonce: undefined,
+        customProperties: undefined,
+        minifyTheme: undefined,
+        themeCache: undefined
+      },
+      themes: {
+        light: {
+          primary: '#D08800',
+          secondary: '#D22509',
+          accent: '#661706',
+          body: 'F6EEC9',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FB8C00'
+        }
+      }
     }
   })
 }
